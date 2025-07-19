29771fb10f888ea1f2a3e28dc3c6ca9021316dac
Vypin1 <karizvix@gmail.com>
2025-07-19 09:40:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;qA0z2YrJ#31JgQLmDhfAEreGKJy-tLa448zbH1qtwJzswh9qMHgw, // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
